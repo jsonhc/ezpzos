1e187f0420431340247c6c9dd2fc8f5abc57224f
yeye-git <59961856+yeye-git@users.noreply.github.com>
2024-08-11 22:40:42 +1000

    Update Routes.tsx

--- EZPZOS.Web/src/Routes.tsx
+++ EZPZOS.Web/src/Routes.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import QRScannerPage from "./pages/QR/QRScannerPage";
-import Menu from "./pages/Menu/Menu";
-import { DafaultMenuRoutesValues } from "ezpzos.core";
-import Profile from "./pages/Profile/Profile";
-import Kitchen from "./pages/Kitchen/Kitchen";
+import Home from "./Pages/Home";
+import QRScannerPage from "./Pages/QR/QRScannerPage";
+import Menu from "./Pages/Menu/Menu";
+import { DefaultMenuRoutesValues } from "ezpzos.core";
+import Profile from "./Pages/Profile/Profile";
 import LoginSignupPage from "./Pages/LoginOrSignup/LoginOrSignup";
-import OTPPage from "./pages/OTPPage/OTPPage";
+import OTPPage from "./Pages/OTPPage/OTPPage";
 import BusinessHome from "./Pages/Kitchen/BuisnessHome";
 const AppRoutes: React.FC = () => (
 	<Routes>
@@ -17,12 +16,12 @@ const AppRoutes: React.FC = () => (
 		<Route path="otp" element={<OTPPage />} />
 
 		<Route path="scan" element={<QRScannerPage />} />
-		<Route path={DafaultMenuRoutesValues.DineInRouteDefaultValue} element={<Menu />} />
-		<Route path={DafaultMenuRoutesValues.TakeAwayRouteDefaultValue} element={<Menu />} />
+		<Route path={DefaultMenuRoutesValues.DineInRouteDefaultValue} element={<Menu />} />
+		<Route path={DefaultMenuRoutesValues.TakeAwayRouteDefaultValue} element={<Menu />} />
 		<Route path="profile" element={<Profile />} />
 
 		{/* Business Routes */}
-		<Route path="businesshome" element={<BusinessHome />} />
+		<Route path="businesshome" element={<BusinessHome BusinessHomePageValues={{IsLoggedIn:true, HomePageButtonList:[],NotificationList:[]}}/>} />
 		
 	</Routes>
 );
